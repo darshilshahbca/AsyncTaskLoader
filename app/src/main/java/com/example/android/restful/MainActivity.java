3ba574c7e94318c6a3184181d966ac5cd517c631
darshilshahbca <darshil.saphcm@gmail.com>
2018-05-23 00:27:13 +0530

    Manage requests with AsyncTaskLoader

--- app/src/main/java/com/example/android/restful/MainActivity.java
+++ app/src/main/java/com/example/android/restful/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.android.restful;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<String> {
 
     TextView output;
 
@@ -18,11 +22,53 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void runClickHandler(View view) {
-
+        getSupportLoaderManager().initLoader(0, null, this).forceLoad();
     }
 
     public void clearClickHandler(View view) {
         output.setText("");
     }
+
+    @Override
+    public Loader<String> onCreateLoader(int id, Bundle args) {
+        output.append("creating loader\n");
+        return new MyTaskLoader(this);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<String> loader, String data) {
+        output.append("loader finished, returned: = " + data + "\n");
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<String> loader) {
+
+    }
+
+
+    private static class MyTaskLoader extends AsyncTaskLoader<String>{
+
+        public MyTaskLoader(Context context) {
+            super(context);
+        }
+
+        @Override
+        public String loadInBackground() {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "from the loader";
+        }
+
+        @Override
+        public void deliverResult(String data) {
+            data += ", delivered";
+            super.deliverResult(data);
+        }
+    }
+
 
 }
